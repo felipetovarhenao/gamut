d50989ee978ccedd76f2dae5977598cc5bfc25b3
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2021-07-03 20:01:31 -0500

    commit for timeit benchmarking

--- 01_build_corpus.py
+++ 01_build_corpus.py
@@ -3,7 +3,7 @@
 #   ---------------------------------------------------------------------- 
 
 # PATHS
-audio_samples = '/Users/felipe-tovar-henao/Documents/Sample collections/_OrchideaSOL2020_release/OrchideaSOL2020/PluckedStrings'
+audio_samples = '/Users/felipe-tovar-henao/Documents/Sample collections/Berklee44v11'
 output_path = '/Users/felipe-tovar-henao/Documents/Camus files/corpora/'
 duration = None
 hop_length = 512
