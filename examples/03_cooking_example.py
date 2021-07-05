3a23ce5f89285d82a500eeb5443ae1c511676ba1
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2021-07-05 10:30:11 -0500

    few example improvements

--- examples/03_cooking_example.py
+++ examples/03_cooking_example.py
@@ -12,18 +12,18 @@ recipe_path = './MyRecipe.camus'
 recipe = dict_from_camus(recipe_path)
 
 # cooking settings
-envelope = [0, 1, 0.9, 0.5, 0] 
-grain_dur = 0.25
-sr = 44100
-stereo_spread = [0.1, 0.9]
-target_mix = [0, 0.5]
+envelope = [0, 1, 0.9, 0.5, 0] # grain amplitude envelope (type: str, int, float or list -- if str, use scipy.signal.windows types) 
+grain_dur = 0.25 # grain duration (type: int, float, or list)
+sr = 44100 # output sampling rate (type: int)
+stereo = [0.1, 0.9] # spread of stereo image (0.0-1.0) (type: int, float or list)
+target_mix = [0, 0.5] # dry/wet mix of input target (0.0-1.0) (type: int, float, or list)
 
 # cook audio recipe
 audio_array = cook_recipe(recipe_dict=recipe,
                         envelope=envelope,
                         grain_dur=grain_dur,
                         sr=sr,
-                        stereo=stereo_spread,
+                        stereo=stereo,
                         target_mix=target_mix)
 
 # set audio output path
