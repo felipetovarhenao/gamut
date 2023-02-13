0a6eefcb96264884febe802c65cf08e53f1010f0
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-02-13 12:02:23 -0500

    v1.0.9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'GAMuT'
 copyright = '2023, Felipe Tovar-Henao'
 author = 'Felipe Tovar-Henao'
-release = '1.0.8'
+release = '1.0.9'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
