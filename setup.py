820d6db95b4df260c2a93a1f515545026bf0dde2
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2023-03-02 12:34:07 -0500

    v1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 long_description = (Path(__file__).parent / "README.md").read_text()
 
 MODULE = 'gamut'
-VERSION = '1.0.10-dev0'
+VERSION = '1.1.0'
 DESCRIPTION = 'Granular Audio Musaicing Toolkit for Python'
 
 
